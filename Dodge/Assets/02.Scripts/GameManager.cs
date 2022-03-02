c15da0bf823e3d3293486b508b98c8ca53ca57b9
강동윤 <davidkang8111@gmail.com>
2022-03-02 14:24:56 +0900

    20220302

--- Dodge/Assets/02.Scripts/GameManager.cs
+++ Dodge/Assets/02.Scripts/GameManager.cs
@@ -1,49 +1,49 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// UI °ü·Ã ¶óÀÌºê·¯¸® »ç¿ëÇÒ·¡~
+// UI ê´€ë ¨ ë¼ì´ë¸ŒëŸ¬ë¦¬ ì‚¬ìš©í• ë˜~
 using UnityEngine.UI;
-// ¾À °ü¸® °ü·Ã ¶óÀÌºê·¯¸® »ç¿ëÇÒ·¡~
+// ì”¬ ê´€ë¦¬ ê´€ë ¨ ë¼ì´ë¸ŒëŸ¬ë¦¬ ì‚¬ìš©í• ë˜~
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // °ÔÀÓ¿À¹ö ½Ã È°¼ºÈ­ÇÒ ÅØ½ºÆ® °ÔÀÓ ¿ÀºêÁ§Æ®
+    // ê²Œì„ì˜¤ë²„ ì‹œ í™œì„±í™”í•  í…ìŠ¤íŠ¸ ê²Œì„ ì˜¤ë¸Œì íŠ¸
     public GameObject gameOverText;
-    // »ıÁ¸ ½Ã°£À» Ç¥½ÃÇÒ ÅØ½ºÆ® ÄÄÆ÷³ÍÆ®
+    // ìƒì¡´ ì‹œê°„ì„ í‘œì‹œí•  í…ìŠ¤íŠ¸ ì»´í¬ë„ŒíŠ¸
     public Text timeText;
-    // ÃÖ°í ±â·ÏÀ» Ç¥½ÃÇÒ ÅØ½ºÆ® ÄÄÆ÷³ÍÆ®
+    // ìµœê³  ê¸°ë¡ì„ í‘œì‹œí•  í…ìŠ¤íŠ¸ ì»´í¬ë„ŒíŠ¸
     public Text recordText;
 
-    // ½ÇÁ¦ »ıÁ¸ ½Ã°£
+    // ì‹¤ì œ ìƒì¡´ ì‹œê°„
     private float surviveTime;
-    // °ÔÀÓ¿À¹ö »óÅÂ
+    // ê²Œì„ì˜¤ë²„ ìƒíƒœ
     private bool isGameover;
 
     void Start()
     {
-        // »ıÁ¸ ½Ã°£°ú °ÔÀÓ¿À¹ö »óÅÂ ÃÊ±âÈ­
+        // ìƒì¡´ ì‹œê°„ê³¼ ê²Œì„ì˜¤ë²„ ìƒíƒœ ì´ˆê¸°í™”
         surviveTime = 0f;
         isGameover = false;
     }
 
     void Update()
     {
-        // °ÔÀÓ¿À¹ö°¡ ¾Æ´Ñ µ¿¾È
+        // ê²Œì„ì˜¤ë²„ê°€ ì•„ë‹Œ ë™ì•ˆ
         if (!isGameover)
         {
-            // »ıÁ¸ ½Ã°£ °»½Å
+            // ìƒì¡´ ì‹œê°„ ê°±ì‹ 
             surviveTime += Time.deltaTime;
-            // °»½ÅÇÑ »ıÁ¸ ½Ã°£À» timeText
-            // ÄÄÆ÷³ÍÆ®¸¦ ÀÌ¿ëÇØ Ç¥½Ã
+            // ê°±ì‹ í•œ ìƒì¡´ ì‹œê°„ì„ timeText
+            // ì»´í¬ë„ŒíŠ¸ë¥¼ ì´ìš©í•´ í‘œì‹œ
             timeText.text = "Time : "+(int)surviveTime;
         }
     }
 
-    // ÇöÀç °ÔÀÓÀ» °ÔÀÓ¿À¹ö »óÅÂ·Î º¯°æÇÏ´Â ¸Ş¼­µå
+    // í˜„ì¬ ê²Œì„ì„ ê²Œì„ì˜¤ë²„ ìƒíƒœë¡œ ë³€ê²½í•˜ëŠ” ë©”ì„œë“œ
     public void EndGame()
     {
-        // ÇöÀç »óÅÂ¸¦ °ÔÀÓ¿À¹ö »óÅÂ·Î ÀüÈ¯
+        // í˜„ì¬ ìƒíƒœë¥¼ ê²Œì„ì˜¤ë²„ ìƒíƒœë¡œ ì „í™˜
         isGameover = true;
     }
 }
